90ddf4218705b181e12a51ba37bfd801a3f56686
da5011 <davidevan3605@gmail.com>
2023-10-12 18:08:50 -0700

    updated

--- python/ising.py
+++ python/ising.py
@@ -11,7 +11,7 @@ from PIL import Image
 ######################
 
 def startProgram():
-    global filePath, simulation_size, temperature, inv_temperature, number_of_sweeps, saveFrames
+    global filePath, simulation_size, temperature, inv_temperature, number_of_sweeps, steps_per_sweep, saveFrames, per_particle
     
     print("**********************")
     print("ISING MAGNET SIMULATOR")
@@ -21,9 +21,12 @@ def startProgram():
     temperature = float(input("Please input the temperature of the simulation [0-10] (float): "))
     inv_temperature = 1/temperature
     number_of_sweeps = int(input("Please input the number of sweeps for the simulation (integer): "))
+    steps_per_sweep = simulation_size*simulation_size
     saveFramesInput = str(input("Would you like to save simulation snapshots as images? This incurs a performance cost. (Y/N) ")).upper()
 
     saveFrames = True if saveFramesInput == "Y" else False
+
+    per_particle = 1/steps_per_sweep 
 
     os.chdir(os.path.dirname(os.path.abspath(__file__))) #Sets CWD to script
     filePath = os.getcwd() + f"\SimulationData\{simulation_size}-{temperature}-{number_of_sweeps}"
@@ -53,7 +56,7 @@ def initialize(gridSize):
         plotModel(isingModel, "Sweep0")
     return isingModel
 
-def calculateEnergy(isingModel):
+def calculateEnergy(isingModel): #heat capacity is the variance in the energy w T
     energy = 0
     nrows = len(isingModel)
     ncols = len(isingModel[0])
@@ -63,7 +66,7 @@ def calculateEnergy(isingModel):
             currentSpin = isingModel[i,j]
             energy += currentSpin*(isingModel[i,(j+1)%ncols] + isingModel[i,(j-1)%ncols] + \
                                    isingModel[(i+1)%nrows,j] + isingModel[(i-1)%nrows,j]) #Energy calculation w/ PBC
-    return int(-energy/2)
+    return int(-0.5*energy)
 
 def spinFlipEnergyChange(isingModel, x_coord, y_coord):
     nrows = len(isingModel)
@@ -80,9 +83,41 @@ def plotModel(model, fileName):
     plt.savefig(filePath + "\\frames\\" + fileName + ".png", dpi = 800, bbox_inches = "tight")
     plt.close()
 
+def compute_AVG_E(energyArray):
+    AVG_e = np.zeros(len(energyArray))
+    for i in range(0, len(energyArray)):
+        AVG_e[i] = np.mean(energyArray[:i+1])
+    return AVG_e
+
+def compute_E_minus_AVG_E_SQUARED(energyArray, avgEnergyArray):
+    E_minus_AVG_E_SQUARED_Array = np.zeros(len(energyArray))
+    for i in range(0, len(energyArray)):
+        E_minus_AVG_E_SQUARED_Array[i] = (energyArray[i]-avgEnergyArray[i])**2
+    return E_minus_AVG_E_SQUARED_Array
+
+def compute_heatCapacity(bigAssArray):
+    return (inv_temperature**2)*compute_AVG_E(bigAssArray)*per_particle
+
+# def compute_E2_AVG(energyArray):
+#     E2_AVG_Array = np.zeros(len(energyArray))
+#     E2_Array = np.zeros(len(energyArray))
+
+#     for i in range(0, len(E2_AVG_Array)):   
+#         E2_Array[i] = energyArray[i]*energyArray[i]
+
+#     E2_AVG_Array = compute_AVG_E(E2_Array)
+#     return E2_AVG_Array
+
+# def compute_AVG_E2(averageEnergyArray):
+#     AVG_E2_Array = np.zeros(len(averageEnergyArray))
+
+#     for i in range(0, len(averageEnergyArray)): 
+#         AVG_E2_Array[i] = averageEnergyArray[i]*averageEnergyArray[i]
+#     return AVG_E2_Array
+
+
 def MonteCarloLoop(isingModel, number_of_sweeps):
     print("SIMULATION START")
-    steps_per_sweep = len(isingModel)*len(isingModel[0])
 
     for sweep in range(0, number_of_sweeps):
         print("Computing Sweep #" + str(sweep+1), end="\r")
@@ -102,8 +137,12 @@ def MonteCarloLoop(isingModel, number_of_sweeps):
     print("Simulation Complete")
     return isingModel
 
-def DataAnalysis(energyArray):
-    simulationData = pd.DataFrame(energyArray, columns=["Energy"])
+def DataAnalysis():
+    averageEnergy = compute_AVG_E(EnergyPerSweep)
+    energyDifference = compute_E_minus_AVG_E_SQUARED(EnergyPerSweep, averageEnergy)
+    heatCapacity = compute_heatCapacity(energyDifference)
+    simulationData = pd.DataFrame({"Energy":EnergyPerSweep, "<E>":averageEnergy,
+                                   "[E-<E>]^2":energyDifference, "Heat Capacity":heatCapacity})
     simulationData.to_excel(filePath + "\SimulationResults.xlsx")
 
     if saveFrames:
@@ -123,4 +162,4 @@ def DataAnalysis(energyArray):
 startProgram()
 my_model = initialize(simulation_size)
 MonteCarloLoop(my_model, number_of_sweeps)
-DataAnalysis(EnergyPerSweep)
+DataAnalysis()
